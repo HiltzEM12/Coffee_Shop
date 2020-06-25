ae23c4796f05ac520934b379bbc5e374433e9ecb
Eric Hiltz <eric.m.hiltz@gmail.com>
2020-06-25 13:35:59 -0400

    Removed the .auth0 suffix from the url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'roofuseat.us.auth0.com', // the auth0 domain prefix
+    url: 'roofuseat.us', // the auth0 domain prefix
     audience: 'coffee', // the audience set for the auth0 app
     clientId: 'dJReaIrxbUbm0kFRliVy0NJsijlKVS0f', // the client id generated for the auth0 app
     callbackURL: 'https://localhost:8100', // the base url of the running ionic application. 
